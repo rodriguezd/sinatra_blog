1b182b111cfb88036bb21fee28f291d438ad831e
David Rodriguez <davidrodriguez212@gmail.com>
2013-07-02 18:12:54 -0400

    Initial commit. Done

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# Gemfile
+source 'https://rubygems.org'
+
+gem "sinatra"
+gem "sqlite3"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "rake"
+
+group :development do
+  gem "shotgun"
+  gem "tux"
+  gem 'debugger'
+end
---
+++ app.rb
@@ -0,0 +1,97 @@
+require 'bundler'
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+set :database, 'sqlite3:///blog.db'
+
+module Blog
+  class App < Sinatra::Application
+
+    get '/' do
+      @posts = Post.order("created_at DESC")
+      erb :"posts/index"
+    end
+
+    # Get the New Post form
+    get "/posts/new" do
+      @title = "New Post"
+      @post = Post.new
+      erb :"posts/new"
+    end
+
+    post "/posts" do
+      @post = Post.new(params[:post])
+      if @post.save
+        redirect "posts/#{@post.id}"
+      else
+        erb :"posts/new"
+      end
+    end
+
+    # Get the individual page of the post with this ID.
+    get "/posts/:id" do
+      @post = Post.find(params[:id])
+      @title = @post.title
+      erb :"posts/show"
+    end
+
+    # Get the Edit Post form of the post with this ID.
+    get "/posts/:id/edit" do
+      @post = Post.find(params[:id])
+      @title = "Edit Form"
+      erb :"posts/edit"
+    end
+
+    put "/posts/:id" do
+      @post = Post.find(params[:id])
+      if @post.update_attributes(params[:post])
+        redirect "/posts/#{@post.id}"
+      else
+        erb :"posts/edit"
+      end
+    end
+
+    # Deletes the post with this ID and redirects to homepage.
+    delete "/posts/:id" do
+      @post = Post.find(params[:id]).destroy
+      redirect "/"
+    end
+
+    # Our About Me page.
+    get "/about" do
+      @title = "About Me"
+      erb :"pages/about"
+    end
+
+    helpers do
+      # If @title is assigned, add it to the page's title.
+      def title
+        if @title
+          "#{@title} -- My Blog"
+        else
+          "My Blog"
+        end
+      end
+
+      # Format the Ruby Time object returned from a post's created_at method
+      # into a string that looks like this: 06 Jan 2012
+      def pretty_date(time)
+       time.strftime("%d %b %Y")
+      end
+
+      def post_show_page?
+        request.path_info =~ /\/posts\/\d+$/
+      end
+
+      def delete_post_button(post_id)
+        erb :_delete_post_button, locals: { post_id: post_id}
+      end
+
+    end
+
+  end
+end
